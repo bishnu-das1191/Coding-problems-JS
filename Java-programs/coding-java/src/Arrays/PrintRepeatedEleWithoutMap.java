9235b4f39df7ae14a19b25e4ef794c66e149a893
bishnu <bishnu.das1191@gmail.com>
2024-08-22 17:31:20 +0530

    Print repeated elements from array

---
+++ Java-programs/coding-java/src/Arrays/PrintRepeatedEleWithoutMap.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class PrintRepeatedEleWithoutMap {
+    public static void main(String[] args) {
+        /**
+         * Print the elements which are repeated in given array
+         * Without using Arrays Sort and Map
+         */
+
+
+
+        int[] arr = {5, 2, 9, 1, 7, 2, 7, 7};
+
+        boolean[] visited = new boolean[arr.length];
+        Arrays.fill(visited, false);
+
+        for(int i=0; i<arr.length; i++){
+
+            if(visited[i]) continue;
+
+            for (int j = i+1; j <arr.length ; j++) {
+                if(arr[i] == arr[j]){
+                    visited[j] = true;
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
